a5b5c28a70d7ecf11f2b1674cc098ed935d85bc6
Tyler Chang <tylerchang888@gmail.com>
2021-10-28 00:13:38 -0600

    added lazy loading for better performnce

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import useFirestore from '../hooks/useFirestore';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 
-const ImageGrid = ({setSelectedImg}) => {
-    
-    const {docs} = useFirestore('images');
-    // console.log(docs);
+const ImageGrid = ({ setSelectedImg }) => {
 
-    return(
+    const { docs } = useFirestore('images');
+
+    return (
         <div className="img-grid">
             {docs && docs.map(doc => (
-                <motion.div 
-                   className="img-wrap" 
-                   key={doc.id}   
-                   onClick={() => setSelectedImg(doc.url)}
-                   whileHover={{opacity: 1, cursor: 'pointer'}}
-                   >
-       
-                   <motion.img 
-                       src={doc.url} alt="Image Expected"
-                       initial={{opacity: 0}}
-                       animate={{opacity: 1}}
+                <motion.div
+                    className="img-wrap"
+                    key={doc.id}
+                    onClick={() => setSelectedImg(doc.url)}
+                    whileHover={{ opacity: 1, cursor: 'pointer' }}
+                >
+
+                    {/* <motion.img
+                        src={doc.url} alt="Image Expected"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                    /> */}
+                    <LazyLoadImage
+                        alt="Image Expected"
+                        //height={image.height}
+                        src={doc.url} // use normal <img> attributes as props
+                    //width={image.width} 
                     />
-               </motion.div>
+
+                </motion.div>
             ))}
         </div>
     )
